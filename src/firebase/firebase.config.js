f5ffbdd039932adb7d6e46fdcfb85c39eb470843
shariyerShazan <shariyershazan1@gmail.com>
2025-07-24 16:11:44 +0600

    firebase add - google login

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
+
+export const googleLogin = async () => {
+  return await signInWithPopup(auth, provider);
+};
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -8,6 +8,7 @@ import { IoMdArrowRoundBack } from "react-icons/io";
 import { useDispatch } from "react-redux";
 import {  setUser } from "../redux/user.slice";
 import { USER_API_END_POINT } from "../utlis/apiEndPoints";
+import { googleLogin } from "../firebase/firebase.config";
 
 function Login() {
   useEffect(() => {
@@ -47,6 +48,30 @@ function Login() {
         setLoadingBtn(false)
     }
   };
+
+
+  const handleGoogleLogin = async () => {
+    try {
+      const result = await googleLogin();
+      const user = result.user;
+      const newUser = {
+        fullName: user.displayName,
+        email: user.email,
+        profilePicture: user.photoURL,
+      };
+
+      const res = await axios.post(`${USER_API_END_POINT}/firebase-login`, newUser, { withCredentials: true });
+
+      if (res.data.success) {
+        dispatch(setUser(res.data.user));
+        toast.success(res.data.message || "Login successful");
+        navigate("/");
+      }
+    } catch (error) {
+      toast.error(error.message || "Google login failed");
+    }
+  };
+
 
   return (
     <div className="w-[90%] mx-auto">
@@ -110,7 +135,16 @@ function Login() {
             >
               {loadingBtn ?<div className="loading loading-spinner loading-md"></div> :"Login"}
             </button>
-            <span className="my-2">
+
+            {/* google login button */}
+           <div className="border-b-1 border-gray-500 w-full">
+           <button onClick={handleGoogleLogin} className="btn my-3 w-full bg-white hover:bg-favone/40 text-black border-[#e5e5e5] ">
+  <svg aria-label="Google logo" width="16" height="16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><g><path d="m0 0H512V512H0" fill="#fff"></path><path fill="#34a853" d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"></path><path fill="#4285f4" d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"></path><path fill="#fbbc02" d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"></path><path fill="#ea4335" d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"></path></g></svg>
+  Login with Google
+</button>
+           </div>
+
+            <span className="my-2 ">
               Don't have account?{" "}
               <Link className="text-green-500" to={"/register"}>
                 register
